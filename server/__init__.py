3895ef509760f62036eae71f3139c98e97c94cd2
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2022-02-21 18:19:25 +0100

    Database
    Implementazione di lettura e scrittura sul database.

--- server/__init__.py
+++ server/__init__.py
@@ -1,3 +1,6 @@
+import datetime
+import logging
+
 from sqlalchemy import Column, Integer, String, ForeignKey, Date
 from sqlalchemy.orm import relationship, declarative_base
 
@@ -9,6 +12,8 @@ __ALL__ = (
     'process_notification',
 )
 
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s')
 
 # Base per l'ORM
 Base = declarative_base()
@@ -39,5 +44,35 @@ class NotificationCounter(Base):
     customer = relationship('Customer')
 
 
+def find_customer_id(session, notification_text):
+    logging.info(f"Ricerca del cliente per la notifica '{notification_text}'")
+    notification_labels = [c.notification_label for c in session.query(Customer).all()]
+    labels = [l for l in notification_labels if l.lower() in notification_text.lower()]
+    logging.debug(f'Labels: {labels}')
+    if len(labels) > 1:
+        logging.debug(f"Notifica con tante label: {labels}")
+        id_customer = None
+    elif len(labels) == 1:
+        customer = session.query(Customer).filter(Customer.notification_label.ilike(labels[0])).first()
+        id_customer = customer.id
+        logging.debug(f"Cliente '{customer.name}' trovato con label '{labels[0]}' per la notifica '{notification_text}'")
+    else:
+        logging.warning(f"Nessun cliente trovato per la notifica '{notification_text}'")
+        id_customer = None
+    return id_customer
+
+
 def process_notification(notification_text, session):
-    pass
+    # Cerco l'ID cliente in base alla/alle label eventualmente presenti
+    # nel messaggio
+    id_customer = find_customer_id(session, notification_text)
+    # Aggiungo la notifica
+    session.add(Notification(body=notification_text, id_customer=id_customer))
+    # Incremento il contatore del cliente
+    if id_customer is not None:
+        query = session.query(NotificationCounter).filter_by(
+            id_customer=id_customer, day=datetime.date.today())
+        if query.count() == 0:
+            session.add(NotificationCounter(id_customer=id_customer, day=datetime.date.today(), num=1))
+        else:
+            query.update({'num': NotificationCounter.num + 1})
